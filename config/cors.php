489d3b36eac50fd61e13a786a879334f693ed002
syayid <syayidsyayid262@gmail.com>
2026-03-24 11:49:17 +0700

    Update cros php

---
+++ config/cors.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:3000', 
+        'http://127.0.0.1:3000',
+        'http://localhost:3001', 
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
